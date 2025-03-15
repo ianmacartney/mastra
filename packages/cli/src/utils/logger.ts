61d05801bc01e470e56709e622c3876442a339ba
Ian Macartney <ianhmacartney@gmail.com>
2025-03-15 16:59:17 -0700

    fix logger type issue

--- packages/cli/src/utils/logger.ts
+++ packages/cli/src/utils/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger } from '@mastra/core/logger';
+import type { Logger } from '@mastra/core/logger';
 
-export const logger = createLogger({
+export const logger: Logger = createLogger({
   name: 'Mastra CLI',
   level: 'debug',
 });
